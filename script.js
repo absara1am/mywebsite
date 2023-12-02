71fa39b70a10d0c9ae48390ae1d0d973cecc89e6
absar-a1am <workwithabsar@gmail.com>
2023-12-02 12:36:38 +0530

    Third-commit

--- script.js
+++ script.js
@@ -1,44 +1,41 @@
-console.log('Its working')
+console.log("Its working");
 
 //for maintaining the theme even after reload
-let theme = localStorage.getItem('theme')
+let theme = localStorage.getItem("theme");
 if (theme == null) {
-    setTheme('light')
+  setTheme("light");
 } else {
-    setTheme(theme)
+  setTheme(theme);
 }
 
-let themeDots = document.getElementsByClassName('theme-dot')
+let themeDots = document.getElementsByClassName("theme-dot");
 for (var i = 0; i < themeDots.length; i++) {
-    themeDots[i].addEventListener('click', function () {
-        let mode = this.dataset.mode
-        console.log('Option clicked: ', mode)
-        setTheme(mode)
-    })
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    console.log("Option clicked: ", mode);
+    setTheme(mode);
+  });
 }
 
 function setTheme(mode) {
-    if (mode == 'light') {
-        document.getElementById('theme-style').href = 'default.css'
-    }
+  if (mode == "light") {
+    document.getElementById("theme-style").href = "default.css";
+  }
 
-    if (mode == 'blue') {
-        document.getElementById('theme-style').href = 'blue.css'
-    }
+  if (mode == "blue") {
+    document.getElementById("theme-style").href = "blue.css";
+  }
 
-    if (mode == 'green') {
-        document.getElementById('theme-style').href = 'green.css'
-    }
+  if (mode == "green") {
+    document.getElementById("theme-style").href = "green.css";
+  }
 
-    if (mode == 'purple') {
-        document.getElementById('theme-style').href = 'purple.css'
-    }
-    //for maintaining the theme even after reload
-    localStorage.setItem('theme', mode)
-
-
+  if (mode == "purple") {
+    document.getElementById("theme-style").href = "purple.css";
+  }
+  //for maintaining the theme even after reload
+  localStorage.setItem("theme", mode);
 }
-
 
 /*
 Yes, you are correct. Let's go through the steps again to clarify:
